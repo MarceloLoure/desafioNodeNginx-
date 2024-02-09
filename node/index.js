4925b464de6be1fb005a6112b4855b5c2faed6e3
Marcelo Lourenço <pangzinn2@gmail.com>
2024-02-09 00:32:04 -0300

    Retirando comentarios

--- node/index.js
+++ node/index.js
@@ -15,23 +15,18 @@ const config = {
 const connection = mysql.createConnection(config);
 
 app.get('/', (req, res) => {
-    // Consulta ao banco de dados para recuperar a lista de nomes
     connection.query('SELECT name FROM people', (error, results) => {
         if (error) {
-            // Se houver um erro na consulta ao banco de dados, envie uma resposta de erro
             res.status(500).send('Erro ao recuperar nomes do banco de dados');
         } else {
-            // Se a consulta for bem-sucedida, mapeie os resultados para extrair os nomes
             const names = results.map(result => result.name);
             
-            // Construa a resposta HTML com a lista de nomes
             let htmlResponse = '<h1>Full Cycle Rocks!</h1><p>Lista de nomes cadastrados:</p><ul>';
             names.forEach(name => {
                 htmlResponse += `<li>${name}</li>`;
             });
             htmlResponse += '</ul>';
 
-            // Envie a resposta HTML completa
             res.send(htmlResponse);
         }
     });
